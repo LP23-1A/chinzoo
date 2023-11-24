f052883990c2055527201e0344d1ac7900a14ca7
ChinzoR <dorjpuntsag.chinzorig@yahoo.com>
2023-11-24 23:50:34 +0800

    sda jaylku bn

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -6,17 +6,54 @@ import Skils from "@/components/skils";
 import Experience from "@/components/Experience";
 import Work from "@/components/Work";
 import Contact from "@/components/Contact";
+import { useState } from "react";
 export default function Home() {
+  const [isDark, setIsDark] = useState(false);
+
+  const clickHandler = () => {
+    setIsDark(!isDark);
+  };
+
   return (
     <section>
+      <button onClick={clickHandler} className="darkmode">
+        dark mode
+      </button>
       <Navbar />
-      <Hero/>
-      <About/>
-      <br/>
-      <Skils/>
-      <Experience/>
-      <Work/>
-      <Contact/>
+      <Hero />
+      <About />
+      <br />
+      <Skils />
+      <Experience />
+      <Work />
+      <Contact />
     </section>
   );
 }
+
+
+
+
+
+
+// export default function Home() {
+
+//   const [isDark, setIsDark] = useState{false}
+
+//    const clickHandler = {} => {
+//     setIsDark(isDark)
+//    }
+//   return (
+//     <section>
+//       <Navbar />
+//       <Hero/>
+//       <About/>
+//       <button onClick={clickHandler} className="darkmode">dark mode</button>
+//       <br/>
+//       <Skils/>
+//       <Experience/>
+//       <Work/>
+//       <Contact/>
+//     </section>
+//   );
+// }

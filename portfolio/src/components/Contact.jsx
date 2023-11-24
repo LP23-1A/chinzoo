fa5bb0c8ceebf78ca5be26bb5d59bab7de09ed61
ChinzoR <dorjpuntsag.chinzorig@yahoo.com>
2023-11-24 21:34:55 +0800

    edt

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -8,29 +8,28 @@ export default function Contact(){
     return(
         <div className="contact">
             <div className="contact-1">
-                <button>Get in touch</button>
+                <button className="conbutt">Get in touch</button>
                 <p>What’s next? Feel free to reach out to me if you're looking for a developer, have a query, or simply want to connect.</p>
             </div>
-            <div>
-            <div className="contact-2">
+            <div className="cont-2">
+                <div className="contact-2">
                      <Icons/>
-                <h2>reachsagarshah@gmail.com</h2>
+                    <h2>reachsagarshah@gmail.com</h2>
                      <Sc/>
-            </div>
-            <div className="contact-2">
-                     <Call/>
-                <h1>+91 8980500565</h1>
-                      <Sc/>
-            </div>
-                <p>You may also find me on these platforms!</p>
-                <div>
-                    <Git/>
-                    <Twitter/>
-                    <Figma/>
                 </div>
+                <div className="contact-2">
+                     <Call/>
+                     <h1>+91 8980500565</h1>
+                      <Sc/>
+                 </div>
             </div>
-            <div>
-
+             <div className="contact-3">
+                <p>You may also find me on these platforms!</p>
+                <div className="icbt">
+                <Git/>
+                 <Twitter/>
+                <Figma/>
+                </div>
             </div>
         </div>
     )

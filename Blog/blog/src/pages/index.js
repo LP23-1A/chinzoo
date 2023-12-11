299fb6cc5b58ec6ddd1a4d25520d8fcce018a77d
Чинзориг Доржпунсаг <23LP6742@G303-CC0156.local>
2023-12-11 11:49:42 +0800

    done

--- Blog/blog/src/pages/index.js
+++ Blog/blog/src/pages/index.js
@@ -3,74 +3,16 @@ import Carsoule from "@/components/Carsoule"
 import Trending from "@/components/Trending"
 import Allblogpost from "@/components/Allblogpost"
 import Footer from "@/components/Footer"
-import axios from "axios"
-// import { useEffect, useState } from "react"
-import { useEffect, useRef, useState } from "react";
-import { useRouter } from 'next/router'
 
-const api = "https://dev.to/api/articles?username=gereltuyamz"
 
 export default function Home() {
-
- const [data, setData] = useState([]);
- const valueRef = useRef('');
- const initData = useRef([]);
- const router = useRouter();
- const getData = async (api) => {
-   let res = await axios.get(api);
-   initData.current = res.data;
-   setData(res.data);
- };
-
- const reset = () => setData(initData.current);
-
- const filter = (name) => setData(() => initData.current.filter((el) => el.tags === name));
- 
- const handler = () => getData("https://dev.to/api/articles");
- 
- const handlerInputValue = (value) => {
-   valueRef.current =  value;
- }
-
- const handlerRouter = (id) => router.push(`id=${id}`)
- useEffect(() => {
-   getData(api);
- }, []);
-
   return (
     <main className="`body max-w-[1917px]`flex justify-center items-center">
         <div className="flex flex-col gap-[100px] justify-center items-center">
           <Navbar/>
           <Carsoule/>
           <Trending/>
-          <div className="w-[1216px] flex flex-wrap gap-[20px] justify-between">
-          <Allblogpost/>
-            {data.map((el, index) => {        
-             return (          
-              <div className="w-[392px] h-[476px] p-[16px] border rounded-[6px]">
-                  <div className="flex">
-                    <div className="flex justify-start items-start flex-col gap-[10px]">
-                      <img className="w-[360px] rounded-[6px] h-[240px]" src={el.social_image}/>
-                      <button className="pt-[4px] pb-[4px] pr-[10px] pl-[10px] bg-[#eef2ff] rounded-[8px] text-[#818cf8]">{el.tag_list}</button>
-                      <h1 className="text-[24px] font-bold">{el.title}</h1>
-                      <p className="text-[16px] text-[#97989F]">{el.readable_publish_date}</p>
-                    </div>
-                  </div>
-              </div>
-              );
-                   
-                })}    
-                 <div style={{ display: "flex", gap: '24px'}}>
-                  <button onClick={handlerRouter}>load more</button>
-                  <button onClick={() => filter('webdev')}>webdev</button>
-                  <button onClick={() => filter('programming')}>programming</button>
-                  <button onClick={() => filter('nextjs')}>nextjs</button>
-                  <br />
-                  <button onClick={reset}>reset</button>
-                </div>  
-
-          </div>
-              <button onClick={handler} className="pt-[12px] pb-[12px] pl-[20px] pr-[20px] rounded-[12px] bg-gray-100 border hover:bg-gray-200">load more</button>
+           <Allblogpost/>
           <Footer/>
         </div>
     </main>

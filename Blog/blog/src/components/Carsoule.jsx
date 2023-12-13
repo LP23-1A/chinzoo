9ca6d4611ffba9f7089e3dad6462d180ac0fcb20
Чинзориг Доржпунсаг <23LP6742@G303-CC0156.local>
2023-12-13 10:43:13 +0800

    done

--- Blog/blog/src/components/Carsoule.jsx
+++ Blog/blog/src/components/Carsoule.jsx
@@ -1,32 +1,45 @@
-import CarsouleImg from "./Icon/CarsouleImg"
-import Arrow from "./Icon/Arrow"
-
-const data = [
-    {
-        category: "technology",
-        title: "Grid system for better Design User Interface",
-        date: "August 20, 2022",
+import { useState } from "react";
+import { CarouselData } from '@/constant/Caruoseldata'
+import filter from '@/components/Allblogpost'
+import { useParams } from "next/navigation";
+const Carousel = () => {
+    const [ activeIndex, setActiveIndex ] = useState(0)
+    const increment = () => {
+        if (activeIndex === 2) {
+            setActiveIndex(0);
+            return;
+        }
+        setActiveIndex((activeIndex) => activeIndex + 1);
     }
-]
-
-export default function Carsoule () {
-    return(
-       <div className="flex w-[1216px] h-[600px] flex-col items-center justify-center relative gap-3 bg-cover rounded-[8px] bg-[url(https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1702857600&Signature=TEJqgZebhD5ngGJk43qte2~qf859bguz1g9w0nc-t-7JwZKqGO9HkQ4f7UsByRTKRdqOFoXLq-GeNWVTxyF9yaI-X8jNnKzpHHPB0mwm5217XLBDvleUnawK8Q52jyuf37X~tBpedwk-oALlTZsspRn-XWzxdTRQOMwtXGLvj~Kv~0VS~Cxf7gUiI44x1oZGIcO9HQbL7oAr57JaTLqKz-5STAIIaCV4bEzUKUSd3x8o89LHrwfVdB1rnbaOm-NrwsFjsCGap2EeymdGc1fM9MLK7cYBadPJIs062VLD9cvqO4LZZ7kzDuD2FWVmWuhODVPANFGn0YJgP78jzOayAg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)]">
-         
-           <>
-            {
-                data.map((el) =>{
-                    return(
-                        <div className="bg-white w-[598px] h-[252px] p-[40px] absolute left-0 bottom-0 flex flex-col justify-between items-start rounded-[8px] m-[12px] ">
-                            <button className="bg-blue-600 text-white rounded-[8px] p-[4px] rounded-mdls">{el.category}</button>
-                            <h1 className="text-[36px] font-bold">{el.title}</h1>
-                            <p className="text-[16px] text-[#97989F]">{el.date}</p>
-                        </div>
-                    )
-                } )
-            }
-           </>
-          
-       </div>
+    const decrement = () => {
+        if (activeIndex === 0) {
+            setActiveIndex(2);
+            return;
+        }
+        setActiveIndex((activeIndex) => activeIndex - 1);
+    }
+    return (
+        <div className="mt-[100px]">
+            {CarouselData.length != 0 && (
+            <div className="w-[1216px] relative text-black rounded-lg">
+                <img src={ CarouselData[activeIndex].url } className="w-full max-h-[650px] rounded-lg" />
+                <div className="bg-white rounded-lg absolute left-0 bottom-0 p-7 flex flex-col gap-1 max-w-[518px] m-3 border border-[#E8E8EA]">
+                    <button onClick={useParams(filter)} className="text-[19px] font-normal bg-[#4B6BFB] p-1 px-2 rounded-md w-fit">
+                        { CarouselData[activeIndex].btn }
+                    </button>
+                    <p className="font-bold text-3xl">
+                        { CarouselData[activeIndex].desc }
+                    </p>
+                    <p className="text-[#97989F] text-xl">{CarouselData[activeIndex].day}</p>
+                </div>
+            </div>
+            )}
+            <div className="flex justify-end gap-4 my-4">
+                <button onClick={decrement} className="border py-[3px] px-[4px] rounded-[4px] hover:text-[#D4A373]">Prev</button>
+                <button onClick={increment} className="border py-[3px] px-[4px] rounded-[4px] hover:text-[#D4A373]">Next</button>
+            </div>
+        </div>
     )
 }
+
+export default Carousel

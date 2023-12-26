d79ddb2534083ca51bfbdb183f4aa649f03524e0
Чинзориг Доржпунсаг <23LP6742@G303-CC0128.local>
2023-12-26 12:56:32 +0800

    s

--- Income-expense-tracker/back-end/index.js
+++ Income-expense-tracker/back-end/index.js
@@ -1,5 +1,30 @@
 import dotenv, { config } from "dotenv"
 import postgres from "postgres"
+import Express from "express"
+import { Pool } from "pg"
 
+dotenv.config();
+const PORT = process.env.PORT 
+const app = Express;
 
-dotenv config()
+app.use(bp.json());
+
+app.post("/createTable" async (_, res) =>{
+    try {
+        `
+        CREATE TABLE IF NOT EXIST users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
+        )
+    `
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log(`App running on port ${PORT}.`);
+});
+
+app.post("/user", async (req, res) =>{
+    const tableQueryText = 
+})
